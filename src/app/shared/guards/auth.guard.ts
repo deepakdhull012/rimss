27d602aa62f8f113038218827292bda92aa407e2
deepak-dhull-02 <126465762+deepak-dhull-02@users.noreply.github.com>
2023-12-30 18:50:03 +0530

    v16

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BannerType } from '../interfaces/client/banner.interface';
 import { BannerService } from '../services/banner.service';
@@ -7,7 +7,7 @@ import { BannerService } from '../services/banner.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, private bannerService: BannerService) {
 
   }
